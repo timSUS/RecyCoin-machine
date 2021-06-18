1f8dc5cfc6c539982d9417118ff203b4c4328e31
KrzysztofZawisla <zawislakrzysztof.zk@gmail.com>
2021-06-18 17:22:19 +0200

    Init

---
+++ global.d.ts
@@ -0,0 +1,4 @@
+declare module "interpolate-html-plugin";
+declare module "duplicate-package-checker-webpack-plugin";
+declare module "minify-json-webpack-plugin";
+declare module "script-ext-html-webpack-plugin";
