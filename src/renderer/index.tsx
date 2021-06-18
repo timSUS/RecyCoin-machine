4ea7a89af53ac65b82319fc8d2320c81ab734675
KrzysztofZawisla <zawislakrzysztof.zk@gmail.com>
2021-06-18 17:25:27 +0200

    Init

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,9 +1,34 @@
-import "v8-compile-cache";
-import { contextBridge, ContextBridge } from "electron";
-import isElectron from "is-electron";
+import App from "~components/App/App";
+import { StrictMode } from "react";
+import { render } from "react-dom";
+import initTranslation from "~utils/initTranslation/initTranslation";
+import { i18n as TranslationType } from "i18next";
 
-const { exposeInMainWorld }: ContextBridge = contextBridge;
+if (process.env.DEVELOPMENT) {
+  const {
+    getLCP,
+    getFID,
+    getCLS,
+    getFCP,
+    getTTFB,
+  }: typeof import("web-vitals") = await import("web-vitals");
+  // eslint-disable-next-line no-console
+  getLCP(console.log);
+  // eslint-disable-next-line no-console
+  getFID(console.log);
+  // eslint-disable-next-line no-console
+  getCLS(console.log);
+  // eslint-disable-next-line no-console
+  getFCP(console.log);
+  // eslint-disable-next-line no-console
+  getTTFB(console.log);
+}
 
-exposeInMainWorld("api", {
-  isElectron: isElectron,
-});
+const root: HTMLElement | null = document.querySelector("#root");
+const tranlation: TranslationType = await initTranslation();
+render(
+  <StrictMode>
+    <App translation={tranlation} />
+  </StrictMode>,
+  root,
+);
